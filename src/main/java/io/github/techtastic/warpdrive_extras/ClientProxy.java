c1ccbf162de697cb24192b8f92af3763ab237091
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-06-07 18:39:47 -0500

    Initial commit

---
+++ src/main/java/io/github/techtastic/warpdrive_extras/ClientProxy.java
@@ -0,0 +1,8 @@
+package io.github.techtastic.warpdrive_extras;
+
+public class ClientProxy extends CommonProxy {
+
+    // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
+    // Don't forget to call the super methods as well.
+
+}
